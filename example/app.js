f8190be9d7648f891d1855edac811f874d5fd67b
Justin Obney <jobney@envoc.com>
2015-05-27 10:55:30 -0500

    chore: update example

--- example/app.js
+++ example/app.js
@@ -1,3 +1,5 @@
+/*global angular */
+
 var app = angular.module('plunker', ['ngSimpleCalendar']);
 
 app.controller('MainCtrl', function($scope) {
@@ -8,26 +10,26 @@ app.controller('MainCtrl', function($scope) {
   }, {
     name: 'bar',
     date: new Date()
-  },{
+  }, {
     name: 'baz',
     date: '6-12-15'
   }];
-  
+
   $scope.changeMonth = changeMonth;
   $scope.monthName = monthName;
-  
-  function monthName(date){
+
+  function monthName(date) {
     var d = new Date(date);
     var months = [
-      'January','February','March',
-      'April','May','June',
-      'July','August','September',
-      'October','November','December',
+      'January', 'February', 'March',
+      'April', 'May', 'June',
+      'July', 'August', 'September',
+      'October', 'November', 'December',
     ];
     return months[d.getMonth()];
   }
-  
-  function changeMonth(offset){
+
+  function changeMonth(offset) {
     var d = new Date($scope.date);
     $scope.date = d.setMonth(d.getMonth() + offset);
   }
